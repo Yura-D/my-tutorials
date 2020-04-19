04858f19b23944b3956733500cb2e276693d262d
Yura Dyachuk <dyachuk.yu@gmail.com>
2020-04-19 23:42:37 +0300

    Added django skeleton, connected to mongodb and create Tutorial and Category models

---
+++ apps/tutorials/admin.py
@@ -0,0 +1,41 @@
+from django.contrib import admin
+from django.utils.html import mark_safe
+
+from .models import Category, Tutorial
+
+
+class TutorialInline(admin.TabularInline):
+    model = Tutorial
+    extra = 1
+    fields = (
+        'name',
+        'link',
+        'comment',
+    )
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+    inlines = (TutorialInline, )
+
+
+@admin.register(Tutorial)
+class TutorialAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'tutorial_link',
+        'short_comment',
+    )
+    
+    search_fields = ('name', 'comment', )
+
+    def tutorial_link(self, obj):
+        return mark_safe(f'<a href="{obj.link}">{obj.link}</a>')
+    
+    def short_comment(self, obj):
+        if obj.comment:
+            return obj.comment[:200]
+    
---
+++ apps/tutorials/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        if self.name:
+            return self.name
+
+
+class Tutorial(models.Model):
+    name = models.CharField(max_length=255)
+    link = models.TextField()
+    comment = models.TextField(null=True, blank=True)
+    category = models.ForeignKey(to=Category,
+                                 on_delete=models.CASCADE)
+    
+    def __str__(self):
+        if self.name:
+            return self.name
