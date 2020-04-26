d739c0fe6f93123a0133ef78c56799432087cbef
Yura Dyachuk <dyachuk.yu@gmail.com>
2020-04-26 04:50:11 +0300

    Change static

--- apps/settings.py
+++ apps/settings.py
@@ -102,9 +102,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/staticfiles/'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 )
